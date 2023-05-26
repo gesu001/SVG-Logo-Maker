31631cd07e0f9c782cfab06e3c59af29aeff4c4c
Ge Su <gegege.su@gmail.com>
2023-05-27 01:18:36 +1000

    updated index.js&shapes.js&shapes.test.js&example files

---
+++ index.js
@@ -0,0 +1,85 @@
+const inquirer = require('inquirer');
+const { join } = require('path');
+const { writeFile } = require('fs/promises');
+const { Square, Circle, Triangle } = require('./lib/shapes.js');
+const { create } = require('domain');
+
+class Svg {
+    constructor() {
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    setTextElement(text, textColor) {
+        this.textElement = `<text x="150" y="170" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`
+    }
+    setShapeElement(shape) {
+        this.shapeElement = shape.render()
+    }
+    render() {
+        return `<svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+        ${this.shapeElement}
+        ${this.textElement}
+        </svg>`
+    }
+}
+
+const questions = [
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter text for the logo (Must no more than 3 charaters):',
+      },
+      {
+          type: 'input',
+          name: 'textColor',
+          message: 'Enter a text color:',
+        },
+      {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape for the logo:',
+      choices: ['Square', 'Circle', 'Triangle'],
+      },
+      {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter a shape color:',
+        },
+    ]
+
+    function writeToFile(data) {
+        var svg = new Svg();
+        svg.setTextElement(data.text, data.textColor);
+        svg.setShapeElement(shape);
+        const svgString = svg.render();
+        writeFile(
+            join(__dirname, 'logo.svg'), 
+            svgString
+            )
+        .then(() => console.log('Generate logo.svg'))
+        .catch((err) => {
+          console.log(err);
+          console.log('Oops. Something went wrong.');
+        });
+    }
+
+    // Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        if(data.shape === 'Square') {
+            shape = new Square()
+        } else if(data.shape === 'Circle') {
+            shape = new Circle()
+        } else {
+            shape = new Triangle()
+        }
+        shape.setColor(data.shapeColor)
+
+        writeToFile(data);
+  });
+}
+
+// Function call to initialize app
+init();
